9917dc06493ac9962bc31cb7184dafe1b73cbc03
Sincere Dixon <sincere@email.sc.edu>
2021-11-24 10:27:39 -0500

    Added menu navigation to other screens and logout

---
+++ CSCEProjectRun2/app/src/main/java/com/example/csceprojectrun2/CurrentCharacters.java
@@ -0,0 +1,68 @@
+package com.example.csceprojectrun2;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+public class CurrentCharacters extends AppCompatActivity {
+    //Initialize variable
+    DrawerLayout drawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_current_characters);
+
+        //Assign variable
+        drawerLayout = findViewById(R.id.drawer_layout);
+    }
+
+    public void ClickMenu(View view) {
+        //Open drawer
+        MainActivity.openDrawer(drawerLayout);
+    }
+
+    public void ClickLogo(View view) {
+        //Close drawer
+        MainActivity.closeDrawer(drawerLayout);
+    }
+
+    public void ClickHome(View view) {
+        //Redirect to home activity
+        MainActivity.redirectActivity(this,MainActivity.class);
+    }
+
+    public void ClickPopularBuilds(View view){
+        //Redirect to Popular Builds activity
+        MainActivity.redirectActivity(this,PopularBuilds.class);
+    }
+
+    public void ClickCommunityBuilds(View view) {
+        //Redirect to Community Builds activity
+        MainActivity.redirectActivity(this,CommunityBuilds.class);
+    }
+
+    public void ClickCurrentCharacters(View view) {
+        //Recreate the Current Characters activity
+        recreate();
+    }
+
+    public void ClickItemBuilder(View view) {
+        //Redirect to Item Builder activity
+        MainActivity.redirectActivity(this,ItemBuilder.class);
+    }
+
+    public void ClickLogout(View view){
+        //Logout and close app
+        MainActivity.logout(this);
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        //Close drawer
+        MainActivity.closeDrawer(drawerLayout);
+    }
+}
---
+++ CSCEProjectRun2/app/src/main/java/com/example/csceprojectrun2/ItemBuilder.java
@@ -0,0 +1,68 @@
+package com.example.csceprojectrun2;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+public class ItemBuilder extends AppCompatActivity {
+    //Initialize variable
+    DrawerLayout drawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_builder);
+
+        //Assign variable
+        drawerLayout = findViewById(R.id.drawer_layout);
+    }
+
+    public void ClickMenu(View view) {
+        //Open drawer
+        MainActivity.openDrawer(drawerLayout);
+    }
+
+    public void ClickLogo(View view) {
+        //Close drawer
+        MainActivity.closeDrawer(drawerLayout);
+    }
+
+    public void ClickHome(View view) {
+        //Redirect to home activity
+        MainActivity.redirectActivity(this,MainActivity.class);
+    }
+
+    public void ClickPopularBuilds(View view){
+        //Redirect to Popular Builds activity
+        MainActivity.redirectActivity(this, PopularBuilds.class);
+    }
+
+    public void ClickCommunityBuilds(View view) {
+        //Redirect to Community Builds activity
+        MainActivity.redirectActivity(this, CommunityBuilds.class);
+    }
+
+    public void ClickCurrentCharacters(View view) {
+        //Redirect to Community Builds activity
+        MainActivity.redirectActivity(this, CurrentCharacters.class);
+    }
+
+    public void ClickItemBuilder(View view) {
+        //Recreate the Item Builder activity
+        recreate();
+    }
+
+    public void ClickLogout(View view){
+        //Logout and close app
+        MainActivity.logout(this);
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        //Close drawer
+        MainActivity.closeDrawer(drawerLayout);
+    }
+}
