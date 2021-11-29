fb81edd398190af199417ce255afe9bffe4b6605
Austin Tindal <49575558+atindal@users.noreply.github.com>
2021-11-29 08:16:38 -0500

    testing new main screen + api fetching

--- CSCEProjectRun2/app/src/main/java/com/example/csceprojectrun2/MainActivity.java
+++ CSCEProjectRun2/app/src/main/java/com/example/csceprojectrun2/MainActivity.java
@@ -4,6 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.view.LayoutInflater;
+import android.widget.ScrollView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
@@ -11,17 +16,93 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.*;
+
+import java.util.concurrent.TimeUnit;
+
 public class MainActivity extends AppCompatActivity {
     //Initialize variable
     DrawerLayout drawerLayout;
+    ScrollView matchContainer;
+
+    String DEV_KEY_NOT_SECURE = "RGAPI-bcc29098-add9-49f9-a91a-072329de3b73";
+    /* SAMPLE ACCOUNT
+    {
+        "puuid": "9IkIogPfGJh-bx_f1KRGZj8AtMWHV_AIO7UFGxlptJ2q7TtlkV90a49FYfYt5HWhKenPapiF6wE-LA",
+        "gameName": "Liquid Goose",
+        "tagLine": "NA1"
+    }
+    */
+
+    public void createMatch() {
+
+    }
+
+    public void renderMatchHistory(ScrollView matchContainer) {
+        System.out.println("rendering matches");
+        LinearLayout linearLayout = matchContainer.findViewById(R.id.match_container_linear_layout);
+
+        // get match data (refactor)
+        try {
+            System.out.println("TESTING -- HTTP!!!!!!!!!!!!!!!!!");
+
+            URL url = new URL("https://americas.api.riotgames.com/tft/match/v1/matches/by-puuid/9IkIogPfGJh-bx_f1KRGZj8AtMWHV_AIO7UFGxlptJ2q7TtlkV90a49FYfYt5HWhKenPapiF6wE-LA/ids?count=10");
+            HttpURLConnection httpConnection = (HttpURLConnection) url.openConnection();
+            httpConnection.setRequestMethod("GET");
+            httpConnection.setRequestProperty("X-Riot-Token", DEV_KEY_NOT_SECURE);
+
+            String line = "";
+
+            InputStreamReader inputStreamReader = new InputStreamReader(httpConnection.getInputStream());
+            BufferedReader bufferedReader = new BufferedReader((inputStreamReader));
+
+            while((line=bufferedReader.readLine())!=null) {
+                System.out.println(line);
+            }
+
+            bufferedReader.close();
+        }
+        catch(Exception e) {
+            System.out.println(e);
+        }
+
+        // clear any existing match tiles
+        linearLayout.removeAllViews();
+
+        // build new match tiles
+        LayoutInflater inflater = getLayoutInflater();
+
+        for(int i=0; i<10; i++) {
+            // creates the card UI element
+            inflater.inflate(R.layout.match_card, linearLayout, true);
+
+            // get and update new card
+            View newMatchCard = linearLayout.getChildAt(i);
+
+            TextView gameType = newMatchCard.findViewById(R.id.gameType);
+            TextView gameLength = newMatchCard.findViewById((R.id.gameLength));
+
+            gameType.setText("Test RANKED");
+            gameLength.setText("Length: " + i);
+        }
+
+        System.out.println("finished rendering matches!!!!!!!!!!!!!!!!!!!");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        System.out.println("onCreate!!!!!!!!!");
+
         //Assign variable
         drawerLayout = findViewById(R.id.drawer_layout);
+        matchContainer = findViewById(R.id.match_container);
+
+        renderMatchHistory(matchContainer);
     }
 
     public void ClickMenu(View view) {
