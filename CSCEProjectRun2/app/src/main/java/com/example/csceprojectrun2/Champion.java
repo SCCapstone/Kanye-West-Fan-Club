aeab8f38f1f9e5ef46cf249506815d58451cf6ce
Doug <stokesdb@email.sc.edu>
2021-12-03 16:50:46 -0500

    testing JSON

---
+++ CSCEProjectRun2/app/src/main/java/com/example/csceprojectrun2/Champion.java
@@ -0,0 +1,16 @@
+package com.example.csceprojectrun2;
+
+public class Champion {
+    String name;
+    Ability ability;
+    int cost;
+    Stats stats;
+
+    Champion(String name, int cost, Ability ability, Stats stats) {
+        this.name = name;
+        this.ability = ability;
+        this.cost = cost;
+        this.stats = stats;
+    }
+
+}
--- CSCEProjectRun2/app/src/main/java/com/example/csceprojectrun2/CurrentCharacters.java
+++ CSCEProjectRun2/app/src/main/java/com/example/csceprojectrun2/CurrentCharacters.java
@@ -1,22 +1,144 @@
 package com.example.csceprojectrun2;
 
 import android.os.Bundle;
+import android.util.JsonReader;
+import android.util.JsonToken;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import org.json.*;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CurrentCharacters extends AppCompatActivity {
     //Initialize variable
     DrawerLayout drawerLayout;
+    List<Champion> championList = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_current_characters);
-
+        try {
+            InputStream input = getApplicationContext().getAssets().open("set6.json");
+            championList = readJsonStream(input);
+        }catch(IOException e) {}
         //Assign variable
         drawerLayout = findViewById(R.id.drawer_layout);
+    }
+
+
+    public List<Champion> readJsonStream(InputStream in) throws IOException {
+        JsonReader reader = new JsonReader(new InputStreamReader(in, "UTF-8"));
+        try {
+            return readChampionsArray(reader);
+        } finally {
+            reader.close();
+        }
+    }
+
+    public List<Champion> readChampionsArray(JsonReader reader) throws IOException {
+        List<Champion> champions = new ArrayList<Champion>();
+
+        reader.beginArray();
+        while (reader.hasNext()) {
+            champions.add(readChampion(reader));
+        }
+        reader.endArray();
+        return champions;
+    }
+
+    public Champion readChampion(JsonReader reader) throws IOException {
+        String cname = "";
+        int cost = -1;
+        Stats stats = null;
+        Ability ability = null;
+
+        reader.beginObject();
+        while (reader.hasNext()) {
+            String name = reader.nextName();
+            if (name.equals("name")) {
+                cname = reader.nextString();
+            } else if (name.equals("cost")) {
+                cost = reader.nextInt();
+            } else if (name.equals("ability")) {
+                ability = readAbility(reader);
+            } else if (name.equals("stats")) {
+                stats = readStats(reader);
+            } else {
+                reader.skipValue();
+            }
+        }
+        reader.endObject();
+        return new Champion(cname, cost, ability, stats);
+    }
+
+    public Ability readAbility(JsonReader reader) throws IOException {
+        String a = "";
+        String adesc = "";
+
+        reader.beginObject();
+        while (reader.hasNext()) {
+            String name = reader.nextName();
+            if (name.equals("name")) {
+                a = reader.nextString();
+            } else if (name.equals("desc")) {
+                adesc = reader.nextString();
+            } else {
+                reader.skipValue();
+            }
+        }
+        reader.endArray();
+        return new Ability(a, adesc);
+    }
+
+    public Stats readStats(JsonReader reader) throws IOException {
+        int armor = -1;
+        double attackSpeed = -1;
+        float critChance = -1;
+        double critMultiplier = -1;
+        int damage = -1;
+        int hp = -1;
+        int initialMana = -1;
+        int magicResist = -1;
+        int mana = -1;
+        int range = -1;
+
+        reader.beginObject();
+        while (reader.hasNext()) {
+            String name = reader.nextName();
+            if (name.equals("armor") && reader.peek() != JsonToken.NULL) {
+                armor = reader.nextInt();
+            } else if (name.equals("attackSpeed") && reader.peek() != JsonToken.NULL) {
+                attackSpeed = reader.nextDouble();
+            } else if (name.equals("critChance") && reader.peek() != JsonToken.NULL) {
+                critChance = (float)reader.nextDouble();
+            } else if (name.equals("critMultiplier") && reader.peek() != JsonToken.NULL) {
+                critMultiplier = reader.nextDouble();
+            } else if (name.equals("damage") && reader.peek() != JsonToken.NULL) {
+                damage = reader.nextInt();
+            } else if (name.equals("hp") && reader.peek() != JsonToken.NULL) {
+                hp = reader.nextInt();
+            } else if (name.equals("initialMana") && reader.peek() != JsonToken.NULL) {
+                initialMana = reader.nextInt();
+            } else if (name.equals("magicResist") && reader.peek() != JsonToken.NULL) {
+                magicResist = reader.nextInt();
+            } else if (name.equals("mana") && reader.peek() != JsonToken.NULL) {
+                mana = reader.nextInt();
+            } else if (name.equals("range") && reader.peek() != JsonToken.NULL) {
+                range = reader.nextInt();
+            } else {
+                reader.skipValue();
+            }
+        }
+        reader.endObject();
+        return new Stats(armor, attackSpeed, critChance, critMultiplier, damage, hp, initialMana, magicResist, mana, range);
     }
 
     public void ClickMenu(View view) {
