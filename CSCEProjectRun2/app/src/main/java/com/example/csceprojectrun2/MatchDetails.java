523fad36a37b5ac30c1f1c9ddda9260405e28f02
Jack Snelgrove <70242022+Vyzeon@users.noreply.github.com>
2021-12-06 00:35:15 -0500

    Update MatchDetails.java
    Api key update

--- CSCEProjectRun2/app/src/main/java/com/example/csceprojectrun2/MatchDetails.java
+++ CSCEProjectRun2/app/src/main/java/com/example/csceprojectrun2/MatchDetails.java
@@ -28,7 +28,7 @@ public class MatchDetails extends AppCompatActivity{
     public static String[] viewMatchData() {
 
 //Take In key puuid and match
-        String APIKEY = "RGAPI-a6755643-28a1-496c-a560-9e9e9e79ee71";
+        String APIKEY = "RGAPI-c9097aeb-6c07-43ab-b8d3-b28c2808b018";
         String PUUID = "HCuIWTxI-m_MifnTtvqiKmDJK1G925Poz9vn9FEV75ufFii8DJwlR53LLmw8979ytmdEPI9BxHpYyA";
         String MATCH = "NA1_4107774217";
 
